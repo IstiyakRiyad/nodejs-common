7d39896680f7ed9240910fab1a8693db6206aef1
Istiyak <istiyak.riyad@gmail.com>
2023-03-17 15:44:14 +0600

    Fixing error handling

--- src/middlewares/errorHandle.ts
+++ src/middlewares/errorHandle.ts
@@ -1,12 +1,18 @@
-import { Request, Response, NextFunction} from "express"
-import ErrorResponse  from "../interfaces/ErrorResponse";
+import { Request, Response, NextFunction } from "express"
+import { CustomError } from "../errors/customError";
+import ErrorResponse from "../interfaces/ErrorResponse";
 
-export function ErrorMessage(error: Error, req: Request, res: Response<ErrorResponse>, next: NextFunction){
+export function ErrorMessage(error: Error, req: Request, res: Response<ErrorResponse>, next: NextFunction) {
     const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
     res.status(statusCode);
 
-    res.json({
-        message: error.message,
-        stack: process.env.NODE_ENV === 'production' ? undefined : error.stack 
+    if (error instanceof CustomError) {
+        return res.json({
+            errors: error.serializeErrors()
+        });
+    }
+
+    res.status(400).send({
+        errors: [{ message: 'Something went wrong' }]
     });
 }
